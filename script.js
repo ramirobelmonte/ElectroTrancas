b9372d137e216f6393f924eb9d6a294a92f2e374
Ramiro Belmonte <ramirobelmon@gmail.com>
2025-03-18 18:52:06 -0300

    probando la web share api

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const url_api = 'https://script.google.com/macros/s/AKfycbziV-OOFZjOliy94MpjIsRh
 
 let main = document.querySelector('main');
 let card_template = document.getElementById('card_template').content;
+let shareButton = document.querySelector('.share');
 
 const request = new Request(url_api);
 
@@ -22,8 +23,8 @@ function sumHtml(item) {
     clone.querySelector('.nombre').textContent = item.nombre
     clone.querySelector('.efectivo').textContent = pesos
 
-    clone.querySelector('.card-action>.wsp').href = 'https://api.whatsapp.com/send?phone=543813343477&text=' + 'Información sobre: ' + item.nombre + ' a precio: ' + pesos
-    clone.querySelector('.card-action>.share').href = '#' + base36(item.nombre)
+    clone.querySelector('.card-action>.wsp').href = 'https://api.whatsapp.com/send?phone=543813343477&text=' + 'Información sobre: ' + item.nombre + '%0A' + 'a precio: ' + pesos
+    // clone.querySelector('.card-action>.share').href = '#' + base36(item.nombre)
 
     main.appendChild(clone)
 }
@@ -31,3 +32,19 @@ function sumHtml(item) {
 function base36(str) {
   return parseInt(str, 36);
 }
+
+shareButton.addEventListener('click', async () => {
+  const url = window.location.href;
+
+  if (navigator.share) {
+    try {
+      await navigator.share({
+        title: document.title,
+        url: url,
+      });
+      console.log('¡URL compartida con éxito!');
+    } catch (error) {
+      console.error('Error al compartir:', error);
+    }
+  }
+})
