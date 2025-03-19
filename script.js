276ddd50b97d16b5e959afc887a958ede26863b3
Ramiro Belmonte <ramirobelmon@gmail.com>
2025-03-18 23:17:21 -0300

    problema con web share api

--- script.js
+++ script.js
@@ -2,49 +2,47 @@ const url_api = 'https://script.google.com/macros/s/AKfycbziV-OOFZjOliy94MpjIsRh
 
 let main = document.querySelector('main');
 let card_template = document.getElementById('card_template').content;
-let shareButton = document.querySelector('.share');
 
 const request = new Request(url_api);
 
 fetch(request)
-    .then((response) => response.json())
-    .then((json)=>{
-        json.forEach(item => {sumHtml(item)});
+.then((response) => response.json())
+.then((json)=>{
+    json.forEach(item => {sumHtml(item)});
+    
+    let a = document.getElementById('share');
+    a.addEventListener('click', async () =>{
+        if (navigator.share) {
+            try {
+                await navigator.share({
+                    title: document.title,
+                    url: url,
+                });
+                console.log('¡URL compartida con éxito!');
+            } catch (error) {
+                console.error('Error al compartir:', error);
+            }
+        }
     })
+})
 
 function sumHtml(item) {
     let clone = card_template.cloneNode(true)
-
+    
     var pesos = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(item.efectivo);
-
+    
     clone.querySelector('.card').id = base36(item.nombre)
-
+    
     clone.querySelector('.categoria').textContent = item.categoria
     clone.querySelector('.nombre').textContent = item.nombre
     clone.querySelector('.efectivo').textContent = pesos
-
+    
     clone.querySelector('.card-action>.wsp').href = 'https://api.whatsapp.com/send?phone=543813343477&text=' + 'Información sobre: ' + item.nombre + '%0A' + 'a precio: ' + pesos
     // clone.querySelector('.card-action>.share').href = '#' + base36(item.nombre)
-
+    
     main.appendChild(clone)
 }
 
 function base36(str) {
-  return parseInt(str, 36);
+    return parseInt(str, 36);
 }
-
-shareButton.addEventListener('click', async () => {
-  const url = window.location.href;
-
-  if (navigator.share) {
-    try {
-      await navigator.share({
-        title: document.title,
-        url: url,
-      });
-      console.log('¡URL compartida con éxito!');
-    } catch (error) {
-      console.error('Error al compartir:', error);
-    }
-  }
-})
