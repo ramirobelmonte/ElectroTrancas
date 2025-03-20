96cd318e35f63a558051fcd93663972fe8a77d38
Ramiro Belmonte <ramirobelmon@gmail.com>
2025-03-20 19:16:09 -0300

    actualizacion de diseño y agregar cuotas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url_api = 'https://script.google.com/macros/s/AKfycbziV-OOFZjOliy94MpjIsRhELkzYOQ7P7rmM41-h3XFwJOUVngnFXY9ao-Ff2JD7bIe-w/exec';
+const url_api = 'https://script.google.com/macros/s/AKfycby6xtHMss9-y8UrrUJslxFSTG1h4gPbWOUfUyRLyQ_3_vuwyTR9YVFXW7IpYjf4C7hkvg/exec';
 
 let main = document.querySelector('main');
 let card_template = document.getElementById('card_template').content;
@@ -14,7 +14,13 @@ fetch(request)
 function sumHtml(item) {
     let clone = card_template.cloneNode(true)
     
-    var pesos = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(item.efectivo);
+    item.efectivo = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.efectivo);
+    item.n1c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.n1c);
+    item.n3c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.n3c);
+    item.n6c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.n6c);
+    item.mv1c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.mv1c);
+    item.mv3c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.mv3c);
+    item.mv6c = new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS", maximumSignificantDigits: 6 }).format(item.mv6c);
     
     for (const x of item.imagenes) {
         let imge = document.createElement('img')
@@ -25,9 +31,16 @@ function sumHtml(item) {
     clone.querySelector('.card').id = base36(item.nombre)
     clone.querySelector('.categoria').textContent = item.categoria
     clone.querySelector('.nombre').textContent = item.nombre
-    clone.querySelector('.efectivo').textContent = pesos
+    clone.querySelector('.efectivo').textContent = item.efectivo
+
+    clone.querySelector('.n1c').textContent = item.n1c;
+    clone.querySelector('.n3c').textContent = item.n3c;
+    clone.querySelector('.n6c').textContent = item.n6c;
+    clone.querySelector('.mv1c').textContent = item.mv1c;
+    clone.querySelector('.mv3c').textContent = item.mv3c;
+    clone.querySelector('.mv6c').textContent = item.mv6c;
     
-    clone.querySelector('.card-action>.wsp').href = 'https://api.whatsapp.com/send?phone=543813343477&text=' + 'Información sobre: ' + item.nombre + '%0A' + 'a precio: ' + pesos
+    clone.querySelector('.card-action>.wsp').href = 'https://api.whatsapp.com/send?phone=543813343477&text=```'+ item.nombre + '%0AEfectivo%3A%20'+ item.efectivo +'%0A%0ATarjeta%20Naranja%20-%20Visa%20Macro%3A%0A1%20cuota%3A%0A'+item.n1c+'%20-%20'+item.mv1c+'%0A3%20cuotas%3A%0A'+item.n3c+'%20-%20'+item.mv3c+'%0A6%20cuotas%3A%0A'+item.n6c+'%20-%20'+item.mv6c+'```'
     // clone.querySelector('.card-action>.share').href = '#' + base36(item.nombre)
     
     main.appendChild(clone)
